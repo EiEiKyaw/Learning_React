b8216da54f72b2b24bb1ad7167a1063cb01895aa
ei_ei_kyaw_97 <eieikyaw20101968@gmail.com>
2024-07-25 22:13:45 +0630

    add note and other files

--- React/adding-interactivity/src/App.js
+++ React/adding-interactivity/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Main from "./adding_event_handlers/Main";
 import EventPropagation from "./event-propagation/Main";
 import ComponentMemory from "./component-memory/Main";
+import RenderAndCommit from "./render-and-commit/Main";
 
 function App() {
   return (
     <div className="App">
       {/* <Main /> */}
       {/* <EventPropagation /> */}
-      <ComponentMemory />
+      {/* <ComponentMemory /> */}
+      <RenderAndCommit />
     </div>
   );
 }
