72a3d543bc4313e941afdaab07c67b3e18dba5e3
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2024-07-31 16:54:32 +0630

    exercise

--- React/adding-interactivity/src/state-as-snapshot/Main.js
+++ React/adding-interactivity/src/state-as-snapshot/Main.js
@@ -1,13 +1,15 @@
 import SettingStateTriggersRenders from "./N1_SettingStateTriggersRenders";
 import TakeASnapshot from "./N2_TakeASnapshot";
 import StateOverTime from "./N3_StateOverTime";
+import NeverChangeWithinRender from "./N4_NeverChangeWithinRender";
 
 export default function Main() {
   return (
     <>
       {/* <SettingStateTriggersRenders /> */}
       {/* <TakeASnapshot /> */}
-      <StateOverTime />
+      {/* <StateOverTime /> */}
+      <NeverChangeWithinRender />
     </>
   );
 }
