edac9847457e2910c2f91db6686ac651bf84e2a8
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2024-08-07 09:56:46 +0630

    commit the remaining exercises

--- React/adding-interactivity/src/state-as-snapshot/Main.js
+++ React/adding-interactivity/src/state-as-snapshot/Main.js
@@ -2,6 +2,7 @@ import SettingStateTriggersRenders from "./N1_SettingStateTriggersRenders";
 import TakeASnapshot from "./N2_TakeASnapshot";
 import StateOverTime from "./N3_StateOverTime";
 import NeverChangeWithinRender from "./N4_NeverChangeWithinRender";
+import TrafficLight from "./N5_Challenge";
 
 export default function Main() {
   return (
@@ -9,7 +10,8 @@ export default function Main() {
       {/* <SettingStateTriggersRenders /> */}
       {/* <TakeASnapshot /> */}
       {/* <StateOverTime /> */}
-      <NeverChangeWithinRender />
+      {/* <NeverChangeWithinRender /> */}
+      <TrafficLight />
     </>
   );
 }
