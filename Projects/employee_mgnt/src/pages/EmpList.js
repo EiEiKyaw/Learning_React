b73bb4c20cbdc85adcf0e8352b75f17a0e0336f0
ei_ei_kyaw_97 <eieikyaw20101968@gmail.com>
2024-08-19 23:25:17 +0630

    add 'employee-list' page and plain 'edit' page

---
+++ Projects/employee_mgnt/src/pages/EmpList.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import BasicTable from "../components/basic-table";
+import DataTable from "../components/data-table";
+
+function EmpList() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://sst.mglt.workers.dev/js/getEK.json"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setData(data["employee"]);
+      console.log(data["employee"]);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div style={{ marginBottom: "50px" }}>
+        <BasicTable data={data} />
+      </div>
+      <div>
+        <DataTable data={data} />
+      </div>
+    </>
+  );
+}
+
+export default EmpList;
--- Projects/employee_mgnt/src/pages/Home.js
+++ Projects/employee_mgnt/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ResponsiveAppBar from "../components/navbar";
+import EmpList from "./EmpList";
+import EmpEdit from "./EmpEdit";
 
 function Home() {
   return (
     <div>
       <ResponsiveAppBar />
+      <div style={{ margin: "20px" }}>
+        {/* <EmpList /> */}
+        <EmpEdit />
+      </div>
     </div>
   );
 }
