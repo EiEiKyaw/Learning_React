bbf635b6fd8a2a2edd11d37be139e0bf4c09e048
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2024-08-19 16:04:12 +0630

    initial commit with navbar

---
+++ Projects/employee_mgnt/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ResponsiveAppBar from "../components/navbar";
+
+function Home() {
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <h1>gu,,,,,</h1>
+    </div>
+  );
+}
+
+export default Home;
